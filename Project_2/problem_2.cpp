9b20816dfd28ec048c3c7827776d2cb512b57383
Mads <madssb@uio.no>
2022-09-20 12:44:20 +0200

    gjorde den tridiagonale matrisen riktig

--- Project_2/problem_2.cpp
+++ Project_2/problem_2.cpp
@@ -18,42 +18,43 @@ int main()
     double h = (x_hat_max - x_hat_min) / N;
     std::cout << "step length h = " << h <<std::endl;
     // defining the diagonal of tridiagonal matrix
-    double a = -1 / std::pow(h, 2);
-    std::cout << "diagonal elements a = " << a << std::endl;
+    
+    double d = 2 / std::pow(h, 2);
+    std::cout << "diagonal elements d = " << d << std::endl;
     for (int i = 0; i < 6; i++)
     {
-        tridiag(i, i) = a;
+        tridiag(i, i) = d;
     }
     // definding the supra and sub-diagonals
-    double d = 2 / std::pow(h, 2);
-    std::cout << "sub/supra diagonal elements d = " << d << std::endl;
+    double a = -1 / std::pow(h, 2);
+    std::cout << "sub/supra diagonal elements a = " << a     << std::endl;
     for (int i = 0; i < 5; i++)
     {
         // defining the subdiagonal
-        tridiag(i + 1, i) = d;
+        tridiag(i + 1, i) = a;
         // defining the supradiagonal
-        tridiag(i, i + 1) = d;
+        tridiag(i, i + 1) = a;
     }
     std::cout << "tridiagonal matrix:\n"
               << tridiag << std::endl;
 
     // finding the numerical eigenvalues and eigvectors
-    arma::vec num_eigval;
-    arma::mat num_eigvec;
-    arma::eig_sym(num_eigval, num_eigvec, tridiag);
+    arma::vec num_eigvals;
+    arma::mat num_eigvecs;
+    arma::eig_sym(num_eigvals, num_eigvecs, tridiag);
     std::cout << "numerical eigvals:\n"
-              << num_eigval << std::endl;
+              << num_eigvals << std::endl;
     std::cout << "numerical eigvecs:\n"
-              << num_eigvec << std::endl;
+              << num_eigvecs << std::endl;
 
     // finding analytical eigenvalues
-    arma::vec anal_eigval = arma::vec(N - 1);
+    arma::vec anal_eigvals = arma::vec(N - 1);
     double pi = 3.1415926535897;
     for (int i = 1; i < 6; i++)
     {
-        anal_eigval(i - 1) = d + 2 * a * std::cos((i * pi) / (N + 1));
+        anal_eigvals(i - 1) = d + 2 * a * std::cos((i * pi) / (N + 1));
     }
 
     std::cout << "analytical eigvals:\n"
-              << anal_eigval << std::endl;
+              << anal_eigvals << std::endl;
 }
