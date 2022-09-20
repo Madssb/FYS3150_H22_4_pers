3a43ec31d22a2b5473ad9fc1ff65b906373635bc
Mads <madssb@uio.no>
2022-09-20 20:43:52 +0200

    small fixes

--- Project_2/problem_2.cpp
+++ Project_2/problem_2.cpp
@@ -8,24 +8,26 @@ int main()
     //
     // initializing the tridiagonal (a,d,a) matrix A
     //
+
     int N = 6;
     int n_rows = N;
     int n_cols = N;
     arma::mat tridiagonal_matrix(n_rows, n_cols);
 
     // finding the step length h
-    double x_hat_max = 1.;      
+    double x_hat_max = 1.;
     double x_hat_min = 0.;
-    double h = (x_hat_max - x_hat_min) / N;
+    int n_steps = N+1;
+    double h = (x_hat_max - x_hat_min) / n_steps;
 
     // defining the diagonal of tridiagonal matrix
-
     double d = 2 / std::pow(h, 2);
     for (int i = 0; i < 6; i++)
     {
         tridiagonal_matrix(i, i) = d;
     }
-    // definding the supra and sub-diagonals
+
+    // defining the supra- and subdiagonals
     double a = -1 / std::pow(h, 2);
 
     for (int i = 0; i < 5; i++)
@@ -35,9 +37,11 @@ int main()
         // defining the supradiagonal
         tridiagonal_matrix(i, i + 1) = a;
     }
+
     //
     // computing the eigenvalues and eigenvectors for A by numerical means
     //
+
     arma::vec num_eigvals;
     arma::mat num_eigvecs;
     arma::eig_sym(num_eigvals, num_eigvecs, tridiagonal_matrix);
@@ -45,7 +49,7 @@ int main()
     // normalizing eigenvectors in terms of units p=1
     arma::mat num_eigvecs_norm = arma::normalise(num_eigvecs, 1);
 
-    // 
+    //
     // solving for the eigenvalues and eigenvalues for A by analytic means
     //
 
@@ -53,7 +57,7 @@ int main()
     double pi = 3.1415926535897;
     for (int i = 0; i < 6; i++)
     {
-        anal_eigvals(i) = d + 2 * a * std::cos(((i+1) * pi) / (N + 1));
+        anal_eigvals(i) = d + 2 * a * std::cos(((i + 1) * pi) / (N + 1));
     }
 
     // finding analytical eigenvectors
@@ -63,7 +67,7 @@ int main()
         for (int row = 0; row < N; row++)
         {
             std::cout << row << std::endl;
-            anal_eigvecs(row, col) = std::sin((col+1) * (row+1) * pi / (N + 1));
+            anal_eigvecs(row, col) = std::sin((col + 1) * (row + 1) * pi / (N + 1));
         }
     }
 
@@ -71,8 +75,8 @@ int main()
     arma::mat anal_eigvecs_norm = arma::normalise(anal_eigvecs, 1);
 
     // manual rescaling such that signs match for the numeric and analytic eigenvectors
-    std::vector<int>  indices_for_rows_that_must_be_inverted= {1,5};
-    for(int index:indices_for_rows_that_must_be_inverted)
+    std::vector<int> indices_for_rows_that_must_be_inverted = {1, 5};
+    for (int index : indices_for_rows_that_must_be_inverted)
     {
         anal_eigvecs_norm.col(index) *= -1;
     }
@@ -80,8 +84,9 @@ int main()
     //
     // finding relative errors for the computed eigenvalues & eigenvectors
     //
-    arma::vec rel_err_eigvals = (num_eigvals- anal_eigvals)/anal_eigvals;
-    arma::mat rel_err_eigvecs = (num_eigvecs_norm - anal_eigvecs_norm)/anal_eigvecs_norm;
+
+    arma::vec rel_err_eigvals = (num_eigvals - anal_eigvals) / anal_eigvals;
+    arma::mat rel_err_eigvecs = (num_eigvecs_norm - anal_eigvecs_norm) / anal_eigvecs_norm;
 
     //
     // section for output of all results
@@ -91,7 +96,9 @@ int main()
               << tridiagonal_matrix << std::endl;
 
     std::cout << "step length h = " << h << std::endl;
+
     std::cout << "sub/supra diagonal elements a = " << a << std::endl;
+
     std::cout << "diagonal elements d = " << d << std::endl;
 
     std::cout << "analytic eigvenvectors (normalized):\n"
@@ -114,7 +121,7 @@ int main()
 
     std::cout << "relative error for eigenvalues:\n"
               << rel_err_eigvals << std::endl;
-            
+
     std::cout << "relative error for eigenvectors:\n"
               << rel_err_eigvecs << std::endl;
 }
