5e09b61f62827cdc95efef0e8b083a78079aeda8
Mads <madssb@uio.no>
2022-09-20 14:05:02 +0200

    calculating analytical eigenvectors.

--- Project_2/problem_2.cpp
+++ Project_2/problem_2.cpp
@@ -16,9 +16,9 @@ int main()
     double x_hat_max = 1.;
     double x_hat_min = 0.;
     double h = (x_hat_max - x_hat_min) / N;
-    std::cout << "step length h = " << h <<std::endl;
+    std::cout << "step length h = " << h << std::endl;
     // defining the diagonal of tridiagonal matrix
-    
+
     double d = 2 / std::pow(h, 2);
     std::cout << "diagonal elements d = " << d << std::endl;
     for (int i = 0; i < 6; i++)
@@ -27,7 +27,7 @@ int main()
     }
     // definding the supra and sub-diagonals
     double a = -1 / std::pow(h, 2);
-    std::cout << "sub/supra diagonal elements a = " << a     << std::endl;
+    std::cout << "sub/supra diagonal elements a = " << a << std::endl;
     for (int i = 0; i < 5; i++)
     {
         // defining the subdiagonal
@@ -38,7 +38,7 @@ int main()
     std::cout << "tridiagonal matrix:\n"
               << tridiag << std::endl;
 
-    // finding the numerical eigenvalues and eigvectors
+    // finding the numerical eigenvalues and eigenvectors
     arma::vec num_eigvals;
     arma::mat num_eigvecs;
     arma::eig_sym(num_eigvals, num_eigvecs, tridiag);
@@ -46,6 +46,11 @@ int main()
               << num_eigvals << std::endl;
     std::cout << "numerical eigvecs:\n"
               << num_eigvecs << std::endl;
+
+    // normalizing eigenvectors in terms of units p=1
+    arma::mat num_eigvecs_norm = arma::normalise(num_eigvecs,1);
+    std::cout << "numerical eigenvectors normalized:\n"
+              << num_eigvecs_norm << std::endl;
 
     // finding analytical eigenvalues
     arma::vec anal_eigvals = arma::vec(N - 1);
@@ -57,4 +62,14 @@ int main()
 
     std::cout << "analytical eigvals:\n"
               << anal_eigvals << std::endl;
+
+    // finding numerical eigenvalues
+    arma::mat anal_eigvecs;
+    for i in range(int i = 1; i < N; i++)
+    {
+        for j in range(int j= 0; i<N-1; i++)
+        {
+            
+        }
+    }
 }
