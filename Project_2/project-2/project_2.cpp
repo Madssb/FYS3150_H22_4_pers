a32400af7054f6fc8ed2b5cc842fc9fd9c7570dc
Johan Carlsen <johancarlsen@Johans-MacBook-Pro.local>
2022-09-20 13:14:52 +0200

    Added the 1/h^2 to (a,d,a)

--- Project_2/project-2/project_2.cpp
+++ Project_2/project-2/project_2.cpp
@@ -3,7 +3,11 @@
 
 int main(){
 
-  ana_vs_arma_test(2., -1., 6.);
+  double h = 1./6;
+  double a = -1./(h*h);
+  double d = 2./(h*h);
+
+  ana_vs_arma_test(d, a, 6.);
 
   max_offdiag_symm_test();
 
