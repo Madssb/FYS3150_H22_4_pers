b6f3947baf26e8369460ca491a92845a5e231806
Mads <madssb@uio.no>
2022-09-21 10:30:50 +0200

    finished up the max_offdiag_symmetric

--- Project_2/problem3a.cpp
+++ Project_2/problem3a.cpp
@@ -1,21 +1,20 @@
 #include <armadillo>
 #include <iostream>
 
-double max_offdiag_symmetric(const arma::mat &A, int &k, int &l)
+double max_offdiag_symmetric(const arma::mat &symmetric_matrix, int &k, int &l)
 {
-    int matrix_dims = A.n_rows();
+    int matrix_dims = symmetric_matrix.n_rows();
     double current_element_sup;
     double max_element_sup = 0; 
-    for (indice = 0; i < matrix_dims() - ; i++)
+    for (i = 0; i < symmetric_matrix.n_cols()  ; i++)
     {
-        current_element_sup = A(i + 1, i)
+        current_element_sup = symmetric_matrix(i + 1, i)
         if(current_element_sup > max_element_sup)
         {
-            row_largest = i+1;
-            col_largest = i;
-            max_element_sup = A(i+1,i);
+            //k and l are decided by the position of the largest element
+            k = i+1;
+            l = i;
+            max_element_sup = symmetric_matrix(i+1,i);
         }
     }
-    k = row_largest;
-    l = col_largest;
 }
