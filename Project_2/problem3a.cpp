cc9a7fa2de927e01cacb588dfc7cff5c81a92d2f
Mads <madssb@uio.no>
2022-09-21 11:19:13 +0200

    idk

--- Project_2/problem3a.cpp
+++ Project_2/problem3a.cpp
@@ -1,7 +1,8 @@
 #include <armadillo>
 #include <iostream>
+#include <cmath>
 
-double max_offdiag_symmetric(const arma::mat &symmetric_matrix, int &k, int &l)
+double maximum_element(const arma::mat &symmetric_matrix, int &k, int &l)
 {
     int matrix_dims = symmetric_matrix.n_rows();
     double current_element_sup;
@@ -9,7 +10,7 @@ double max_offdiag_symmetric(const arma::mat &symmetric_matrix, int &k, int &l)
     for (i = 0; i < symmetric_matrix.n_cols()  ; i++)
     {
         current_element_sup = symmetric_matrix(i + 1, i)
-        if(current_element_sup > max_element_sup)
+        if(std::abs(current_element_sup) > std::abs(max_element_sup))
         {
             //k and l are decided by the position of the largest element
             k = i+1;
@@ -17,4 +18,5 @@ double max_offdiag_symmetric(const arma::mat &symmetric_matrix, int &k, int &l)
             max_element_sup = symmetric_matrix(i+1,i);
         }
     }
+    return max_element_sup
 }
