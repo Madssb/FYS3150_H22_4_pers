e154803f3ee9a29c4519196e4f7f2a2cda4d46de
JohanCarlsen <johan_carlsen@hotmail.com>
2022-10-22 16:03:56 +0200

    Changed the plot title of one plot that was incorrect.

--- Project_3/particles_remaining.py
+++ Project_3/particles_remaining.py
@@ -1,6 +1,6 @@
 # Plot how many particles remains after a 500 mu*s simulation.
 # This program will use the no_of_particles.txt file created by
-# the program main.cpp. 
+# the program main.cpp.
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,8 +10,8 @@ f1, f2, f3 = _f1[0], _f2[0], _f3[0]
 w_V, N1, N2, N3 = np.loadtxt('no_of_particles.txt', usecols=(0, 4, 5, 6), unpack=True)
 
 plt.figure(figsize=(8, 4.5))
-# plt.title('Particles in trap after $500\,\mu s$\nwithout particle interactions')
-plt.title('Particles in trap after $500\,\mu s$\nwith particle interactions')
+plt.title('Particles in trap after $500\,\mu s$\nwithout particle interactions')
+# plt.title('Particles in trap after $500\,\mu s$\nwith particle interactions')
 plt.plot(w_V, N1, color='red', lw=1, label=f'$f={f1:.1f}$')
 # plt.plot(w_V, N2, color='royalblue', lw=1, label=f'$f={f2:.1f}$')
 # plt.plot(w_V, N3, color='black', lw=1, label=f'$f={f3:.1f}$')
