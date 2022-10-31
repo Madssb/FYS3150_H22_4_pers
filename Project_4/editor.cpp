8a64424edaa39a02e0ce5ea42d839500908f8e9f
aliras <$ aliras@uio.no>
2022-10-31 10:00:14 +0100

    main

---
+++ Project_4/editor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include <cmath>
+#include <cstdlib> 
+
+#define ARMA_DONT_USE_STD_MUTEX
+
+#include <armadillo>
+
+
+
+int main()
+{
+
+    
+
+
+}
